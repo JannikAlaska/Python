427b6e577ea7f372e1bcaa29d41cf4bb16acc570
Jannik Berthold <Jannik@berthold-ilsenburg.de>
2020-06-29 02:42:08 -0400

    TicTacToe v.2

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -34,13 +35,13 @@ class Board:
 		return False
 
 	def is_valid_turn(self, cell):
-		if state[cell] == 0:
+		if self.state[cell] == 0:
 			return True
 		else:
 			return False
 
 	def check_win(self, Player):
-		s = Player.sybmol
+		s = Player.symbol
 		if self.state[0] == s and self.state[1] == s and self.state[2] == s:
 			return True
 		elif self.state[3] == s and self.state[4] == s and self.state[5] == s:
@@ -75,9 +76,9 @@ class Board:
 
 	
 	def print_board(self):
-			print(" " + self.sign_to_printable(self.state[0] + " | " + self.sign_to_printable(self.state[1] + " | " + self.sign_to_printable(self.state[2] + " \n" +
-    			" " + self.sign_to_printable(self.state[3] + " | " + self.sign_to_printable(self.state[4] + " | " + self.sign_to_printable(self.state[5] + " \n" +
-    			" " + self.sign_to_printable(self.state[6] + " | " + self.sign_to_printable(self.state[7] + " | " + self.sign_to_printable(self.state[8] + " \n")
+			print(" " + self.sign_to_printable(self.state[0]) + " | " + self.sign_to_printable(self.state[1]) + " | " + self.sign_to_printable(self.state[2]) + " \n" +
+    			" " + self.sign_to_printable(self.state[3]) + " | " + self.sign_to_printable(self.state[4]) + " | " + self.sign_to_printable(self.state[5]) + " \n" +
+    			" " + self.sign_to_printable(self.state[6]) + " | " + self.sign_to_printable(self.state[7]) + " | " + self.sign_to_printable(self.state[8]) + " \n")
 
 
 
@@ -85,21 +86,23 @@ class Board:
 
 # ---Main----------------------------------------------------------------------------------------------------------
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
+	player_a = " "
+	player_b = " "
 	player_a = Player(1)
 	player_b = Player(-1)
 	board = Board()
 	active_player = player_a
 
 	while not board.is_full():
-		Board.print_board()
+		board.print_board()
 		try:
-			cell = int(input("Wo willst du dein Zeichen setzen? [1 - 9]")
+			cell = int(input("Wo willst du dein Zeichen setzen? [1 - 9]"))
 		except ValueError:
 			continue
 		cell = cell - 1
 
-		if cell < 0 or cell > 8
+		if cell < 0 or cell > 8:
 			print ("Bitte geben Sie eine Nummer zwischen 1 und 9 ein")
 			continue
 		
@@ -110,7 +113,7 @@ if __name__ == "__main__":
 		if board.check_win(active_player):
 			print("Du hast gewonnen!")
 
-		if active player == player_a:
+		if active_player == player_a:
 			active_player = player_b
 		else:
 			active_player = player_a
